71348715a1a8cfc2b44c8e69be2d8323f8ab6f58
fsdkfklsfmlskfma <105391511+fsdkfklsfmlskfma@users.noreply.github.com>
2022-05-16 00:14:06 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import gtx750 from './components/gtx750.vue'
+
+  export default {
+    components:{
+      gtx750,
+
+    }
+
+  }
+
+</script>
+
+<template>
+  <div id="root">
+    <gtx750>
+      dfskdkag;fl
+    </gtx750>
+
+
+
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/gtx750.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main">
+        <div class="main-card"></div>
+    dslkfdsal;dasld;ls
+    </div>  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.main-card {
+
+    font-family: 'Kanit', sans-serif;
+    color: #00FF29;
+    
+    background-color: black;
+
+    margin-top: 30px;
+    margin-right: 62px;
+    
+    width: 220px;
+    height: 350px;
+}
+</style>
